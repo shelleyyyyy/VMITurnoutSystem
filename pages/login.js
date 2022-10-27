ce3de601cd48b8ad80e273ac5c190bd5280e1bad
Ethan Chanco <110587990+chancoes@users.noreply.github.com>
2022-10-26 20:06:31 -0400

    reworked login GUI using DaisyUI

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import LoginBox from "./components/LoginBox"
 
 
 
@@ -12,18 +10,7 @@ export default function Login() {
     }
 
     return (
-      <div>
-            <h1>VMI Turnout System Login</h1>
-            <label for ="lblEmail">E-Mail: </label>
-            <input type ="text" id="txtEmail" name="txtEmail" placeholder=" "></input>
-            <label for ="lblPassword">Password: </label>
-            <input type ="text" id="txtPassword" name="txtPassword" placeholder=" "></input>
-            <button type="button">Login</button>
-            <br></br>
-            <a href=" ">Sign Up Here</a>
-            <br></br>
-            <a href=" ">Forgot Password?</a>
-      </div>
+      <LoginBox></LoginBox>
     )
   }
   
