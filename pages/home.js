b6a2907a67f4c938aa088b4e06e6244eedc05412
khoaleeee <dangkhoale176@gmail.com>
2022-11-09 21:14:47 -0500

    home page

--- pages/home.js
+++ pages/home.js
@@ -4,18 +4,51 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
+  let name = "Welcome to the VMI TURNOUT"
+
+  let posts = [
+    {
+      "title": "The first one",
+      "content": "testing content 123"
+    },
+    {
+      "title": "test title",
+      "content": "testing content 123"
+    },
+  ]
+
+  
+
+  function Card({title, content}){
+    return(
+      <div>
+        <h1>
+          {title}
+        </h1>
+        <h1>{content}</h1>
+      </div>
+    )
+  }
+
 return(
 
-<div>
-  
-    VMI TurnOut
-  
-  
-<img src = "https://www.insightintodiversity.com/wp-content/uploads/2020/12/VMI.jpg">
+  <div>
+    
+        {name}
 
-</img>
+      
+    <img src = "https://www.insightintodiversity.com/wp-content/uploads/2020/12/VMI.jpg">
 
-</div>
+        
+    </img>
+
+
+    {posts.map((k) => {
+      // return <Card title={k.title} content={k.content}></Card>
+      return <div>{k.title}</div>
+    })}
+
+  </div>
 )
-
+  
   }
